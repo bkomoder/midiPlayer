8c9c1674b26d0b7b86da745cab742f80911f0640
bkomoder <bartoszkomoder@gmail.com>
2017-10-19 01:30:56 +0200

    changed default window size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MidiPlayer");
-        primaryStage.setScene(new Scene(root, 1200, 300));
+        primaryStage.setScene(new Scene(root, 1200, 500));
         primaryStage.show();
     }
 
